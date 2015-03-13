7541f53f2c99af05c9ea8fc6451ffbf2b19e323e
Rodrigo.Ruz.V@gmail.com <Rodrigo.Ruz.V@gmail.com@8b569082-685f-36e1-2200-aa285e3d0ae4>
2015-03-13 18:22:32 +0000

    Updated Eurekalog version Updated Delphi Version Updated Copyright

--- DelphiPreviewHandler.dpr
+++ DelphiPreviewHandler.dpr
@@ -1,27 +1,35 @@
-{**************************************************************************************************}
-{                                                                                                  }
-{ DelphiPreviewHandler.dproj                                                                       }
-{ Project file for the Delphi Preview Handler                                                      }
-{                                                                                                  }
-{ The contents of this file are subject to the Mozilla Public License Version 1.1 (the "License"); }
-{ you may not use this file except in compliance with the License. You may obtain a copy of the    }
-{ License at http://www.mozilla.org/MPL/                                                           }
-{                                                                                                  }
-{ Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF   }
-{ ANY KIND, either express or implied. See the License for the specific language governing rights  }
-{ and limitations under the License.                                                               }
-{                                                                                                  }
-{ The Original Code is DelphiPreviewHandler.dproj.                                                 }
-{                                                                                                  }
-{ The Initial Developer of the Original Code is Rodrigo Ruz V.                                     }
-{ Portions created by Rodrigo Ruz V. are Copyright (C) 2011 Rodrigo Ruz V.                         }
-{ All Rights Reserved.                                                                             }
-{                                                                                                  }
-{**************************************************************************************************}
+//**************************************************************************************************
+//
+// Unit DelphiPreviewHandler
+// unit for the Delphi Preview Handler
+//
+// The contents of this file are subject to the Mozilla Public License Version 1.1 (the "License");
+// you may not use this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.mozilla.org/MPL/
+//
+// Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
+// ANY KIND, either express or implied. See the License for the specific language governing rights
+// and limitations under the License.
+//
+// The Original Code is DelphiPreviewHandler.dpr
+//
+// The Initial Developer of the Original Code is Rodrigo Ruz V.
+// Portions created by Rodrigo Ruz V. are Copyright (C) 2011-2015 Rodrigo Ruz V.
+// All Rights Reserved.
+//
+//*************************************************************************************************
+
 
 library DelphiPreviewHandler;
 
 uses
+  EMemLeaks,
+  EResLeaks,
+  ESendMailSMAPI,
+  EDebugExports,
+  EDebugJCL,
+  EMapWin32,
+  ExceptionLog7,
   ComServ,
   Main in 'Main.pas',
   uEditor in 'units\uEditor.pas' {FrmEditor},
